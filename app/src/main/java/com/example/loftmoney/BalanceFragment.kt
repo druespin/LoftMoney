bf5d2da78e8ab2cd7450e70e0a007e3a438eea2a
druespin <wasisdust@gmail.com>
2020-04-18 14:18:44 +0300

    fragment created

---
+++ app/src/main/java/com/example/loftmoney/BalanceFragment.kt
@@ -0,0 +1,24 @@
+package com.example.loftmoney
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+
+class BalanceFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_balance, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
